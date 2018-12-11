83712e633ee469a2151c81c282950dfc6a8e1a5d
sholtkamp <S.Kokon93@gmail.com>
2018-12-11 12:55:17 +0100

    init

---
+++ Iceland.js
@@ -0,0 +1,36 @@
+//initiate map
+var map = L.map('map_div', { zoomControl: false });
+map.setView([51.9606649, 7.6261347], 13);
+
+//add tile layer to map
+var map_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+map.addLayer(map_layer);
+
+// add zoom control in right top corner
+L.control.zoom({
+  position: 'topright'
+}).addTo(map);
+
+
+// add search in top left corner
+map.addControl(new L.Control.Search({
+  url: 'http://nominatim.openstreetmap.org/search?format=json&q={s}',
+  jsonpParam: 'json_callback',
+  propertyName: 'display_name',
+  propertyLoc: ['lat', 'lon'],
+  marker: L.marker([0, 0]),
+  autoCollapse: true,
+  autoType: false,
+  minLength: 2
+}));
+
+//add location finder
+var lc = L.control.locate({
+  position: 'topleft',
+  strings: {
+    setView: "once"
+  }
+}).addTo(map);
