73b6939c400dfa7379b033f13be7e855211d67ff
sholtkamp <S.Kokon93@gmail.com>
2019-01-31 11:15:29 +0100

    Update map_setup.js

--- src/js/map_setup.js
+++ src/js/map_setup.js
@@ -3,11 +3,11 @@ var map = L.map('map_div', { zoomControl: false });
 map.setView([64.759782, -18.423403], 6.4);
 
 //add tile layer to map
-var map_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var mapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-map.addLayer(map_layer);
+map.addLayer(mapLayer);
 
 // add zoom control in right top corner
 L.control.zoom({
@@ -33,3 +33,15 @@ map.addControl(new L.Control.Search({
   autoType: false,
   minLength: 2
 }));
+
+// define layers here
+// var cities = L.tileLayer(, {id: '', attribution: }),
+
+var overlayMaps = {
+  "Cities" : placeLayer,
+  "Volcanos" : volcanoLayer,
+  "Waterbodies" : waterLayer,
+  "Natural Features" : natureLayer,
+}
+
+L.control.layers(mapLayer).addTo(map);
